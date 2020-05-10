d646ec0f9b33ec8e819ac9a1139190484681979a
Isabel Linracova <isabel.linracova@gmail.com>
2020-05-10 14:20:00 -0400

    add more news

--- rss_parse.rb
+++ rss_parse.rb
@@ -4,22 +4,22 @@ require 'rss'
 require 'open-uri'
 require 'reverse_markdown'
 
-  url = 'https://newsfeed.eveonline.com/en-US/42/articles' #/page/1/20'
-  URI.open(url) do |rss|
-    feed = RSS::Parser.parse(rss)
-    feed.items.each do |item|
-      title = item.title.content
-      author = item.author.name.content
-      published = item.published.content
-      link = item.link.href
-      content = ReverseMarkdown.convert(item.content.content)
-      adjusted_publish = published.to_s.gsub(/[-T:Z]/,"")
-      File.open("news/#{adjusted_publish}.md", "w") do |file|
-        file.puts "# #{title}"
-        file.puts "By #{author}"
-        file.puts "Published on #{link} at #{published}"
-        file.puts ""
-        file.puts content
-      end
+url = "https://newsfeed.eveonline.com/en-US/42/articles/page/1/1200"
+URI.open(url) do |rss|
+  feed = RSS::Parser.parse(rss)
+  feed.items.each do |item|
+    title = item.title.content
+    author = item.author.name.content
+    published = item.published.content
+    link = item.link&.href
+    content = ReverseMarkdown.convert(item.content.content)
+    adjusted_publish = published.to_s.gsub(/[-T:Z]/,"")
+    File.open("news/#{adjusted_publish}.md", "w") do |file|
+      file.puts "# #{title}"
+      file.puts "By #{author}"
+      file.puts "Published on #{link} at #{published}"
+      file.puts ""
+      file.puts content
     end
   end
+end
